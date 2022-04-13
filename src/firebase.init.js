dad5d5bb4a556fcf1cdb92d4fdd9095de76b5b3a
md-sajim <mdabutaliveb@gmail.com>
2022-04-13 15:29:42 +0600

    git intilization

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5Q_RKKbUI059LC1TrrkG5Uw03dNG59uc",
+  authDomain: "rauting-react-intarogation2.firebaseapp.com",
+  projectId: "rauting-react-intarogation2",
+  storageBucket: "rauting-react-intarogation2.appspot.com",
+  messagingSenderId: "345792041916",
+  appId: "1:345792041916:web:69b1793ae62f5bbabe51b9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
